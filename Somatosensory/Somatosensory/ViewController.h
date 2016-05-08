a68bebe892ec43710eabff8fd49039577789a8e7
huanghuacai <18811108252@163.com>
2016-05-08 08:17:49 +0800

    初始化项目

---
+++ Somatosensory/Somatosensory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Somatosensory
+//
+//  Created by 蔡万鸿 on 16/5/8.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Somatosensory/Somatosensory/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Somatosensory
+//
+//  Created by 蔡万鸿 on 16/5/8.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
